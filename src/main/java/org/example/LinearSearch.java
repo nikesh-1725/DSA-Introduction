2c49104f289f3f41b9083d0d6e5a602993994782
nikesh <nikeshchimula417@gmail.com>
2026-01-31 11:19:18 +0530

    LinearSearch Example

---
+++ src/main/java/org/example/LinearSearch.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class LinearSearch
+{
+    public static void main( String[] args )
+    {
+        int[] a={1,4,5,6,8,11};
+        Scanner sc=new Scanner(System.in);
+        int val=sc.nextInt();
+        int result=findIndexOfNumber(val, a);
+        if(result!= -1)
+            System.out.println("Found the index: "+result);
+        else{
+            System.out.println("Not found index: ");
+        }
+    }
+
+    private static int findIndexOfNumber(int val, int[] a) {
+        for(int i=0;i<a.length;i++){
+            if(a[i]==val){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
