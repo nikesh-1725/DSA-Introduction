d4342febba0d45a9d718266182a83e7fb1d6a6b2
Nikesh Chimula <nikchimu@sutherlandglobal.com>
2026-02-08 12:26:38 +0530

    InsertionSort

--- src/main/java/org/example/SortingTechniques/InsertionSort.java
+++ src/main/java/org/example/SortingTechniques/InsertionSort.java
@@ -1,7 +1,27 @@
 package org.example.SortingTechniques;
 
+import java.util.Arrays;
+
 public class InsertionSort {
     public static void main(String[] args) {
+        int[] a={5,6,8,1,3,2,0,-1};
+        System.out.println("Sorted using Selection Sort: "+ Arrays.toString(insertionSort(a, a.length)));
+    }
 
+    private static int[] insertionSort(int[] a, int len) {
+
+
+        for(int i=1;i<len;i++){
+           int key=a[i];
+           int j=i-1;
+            while (j>=0 && a[j]<key){
+                a[j+1]=a[j];
+                j--;
+            }
+            a[j+1]=key;
+
+        }
+
+        return a;
     }
 }
